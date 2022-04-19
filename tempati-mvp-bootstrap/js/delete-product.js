00e404441e06ee8ade3028582f4ae5be41e33d35
bimantaramajid <bimantara1104@gmail.com>
2022-04-19 13:50:18 +0700

    [Tempati] mvp with bootstrap #2

---
+++ tempati-mvp-bootstrap/js/delete-product.js
@@ -0,0 +1,24 @@
+var delete_product = () => {
+    Swal.fire({
+        // title: 'Are you sure?',
+        // text: "Item will not recovery after delete",
+        // icon: 'warning',
+        html:
+        '<img src="../img/icon-trash.svg" alt="deleted">'+
+        '<div class="title-1">Are Your Sure ?</div>'+
+        '<div class="label-r">Item will not recovery after delete</div>',
+        showCancelButton: true,
+        confirmButtonColor: '#0F87FF',
+        cancelButtonColor: '#EF233C',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+      
+}
